Sensor point matching with Hilber curve indices, Non-Linear least squares ICP imlementation, Fix rotation matrix

playground/environment/body.py
@@ -1,5 +1,5 @@
 import numpy as np
-from playground.utils.transform import create_rotation_matrix, make_direction
+from playground.utils.transform import create_rotation_matrix_yx, make_direction
 
 
 class Body:
@@ -12,7 +12,7 @@ class Body:
         self.__rotation_matrix = np.identity(3)
 
     def rotate(self, angle):
-        tr_mat = create_rotation_matrix(angle)
+        tr_mat = create_rotation_matrix_yx(angle)
         self.__rotation_matrix = np.matmul(self.__rotation_matrix, tr_mat)
 
     def move(self, dist):

playground/sensor.py
@@ -28,6 +28,7 @@ class Sensor:
     def scan(self, position, rotation, world):
         # do ray tracing
         obstacles_coords = []
+        obstacles_ids = []
         start_pos = position
         direction = make_direction(rotation)
         for circle_dir in self.__circle_coords:
@@ -40,12 +41,15 @@ class Sensor:
                 end_pos = end_pos.astype(int)
                 ys, xs, _ = line_aa(start_pos[0], start_pos[1], end_pos[0], end_pos[1])
                 for pos in zip(ys, xs):
-                    if world.is_obstacle(pos):
+                    is_obstacle, obstacle_id = world.is_obstacle(pos)
+                    if is_obstacle:
                         obstacles_coords.append(pos)
+                        obstacles_ids.append(obstacle_id)
                         break
 
         if len(obstacles_coords) > 0:
             obstacles_coords = np.array(obstacles_coords)
+            obstacles_ids = np.array(obstacles_ids).reshape((-1, 1))
             # Transform obstacles into the sensor/robot coordinate system
             obstacles_coords -= start_pos
             obstacles_coords = transform_points(obstacles_coords, np.linalg.inv(rotation))
@@ -54,7 +58,7 @@ class Sensor:
             noise = np.random.normal(self.__mu, self.__sigma, size=obstacles_coords.shape)
             obstacles_coords += noise.astype(int)
 
-            self.__obstacles = obstacles_coords
+            self.__obstacles = np.hstack([obstacles_coords, obstacles_ids])
         else:
             self.__obstacles = None
 

playground/slam/frontend.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pygame
 from skimage.draw import line_aa
+from sklearn.neighbors import NearestNeighbors
 
 from playground.slam.frame import Frame
 from playground.slam.icp import ICP
@@ -20,6 +21,26 @@ class FrontEnd:
         self.__frame_align_error = 10  # distance in pixels
         self.__icp = ICP()
         self.__frames = []
+        self.__knn = NearestNeighbors(n_neighbors=1)
+
+    def __find_frames_correspondences(self, frame_a, frame_b, min_dist=5):
+        ids_a = frame_a.observed_points[:, 2]
+        ids_a = ids_a.reshape((-1, 1))
+        ids_b = frame_b.observed_points[:, 2]
+        ids_b = ids_b.reshape((-1, 1))
+        estimator = self.__knn.fit(ids_b)
+        distances, indices = estimator.kneighbors(ids_a, return_distance=True)
+        # remove outliers
+        idx_a = []
+        idx_b = []
+        used_ids = set()
+        for d, i_b, i_a in zip(distances, indices.reshape((-1)).tolist(), range(ids_a.shape[0])):
+            if i_b not in used_ids:
+                if d <= min_dist:
+                    idx_a.append(i_a)
+                    idx_b.append(i_b)
+            used_ids.add(i_b)
+        return idx_a, idx_b
 
     def take_measurements(self, odometry, sensor):
         obstacles = sensor.get_obstacles()
@@ -27,19 +48,21 @@ class FrontEnd:
             # create key frame
             frame_candidate = Frame(odometry.position, odometry.rotation, obstacles.copy())
             if len(self.__frames) > 0:
-                # check if we can align current frame with the last ones
-                for key_frame in self.__frames[:-(self.__last_num_to_check+1):-1]:
-                    rot, pos, align_error = self.__icp.find_transform(frame_candidate, key_frame)
-                    rot_angle = get_rotation_angle(rot)
-                    # skip if position was changed < 1px and rotation < 1 degree
-                    if pos.mean() < 1. and rot_angle < 1.:
-                        return  # we already have the same frame
-                    if align_error < self.__frame_align_error:
-                        # if we were able to align - add new key frame
-                        frame_candidate.position = pos
-                        frame_candidate.rotation = rot
-                        self.__frames.append(frame_candidate)
-                        break
+                # check if we can align current frame with the last one
+                key_frame = self.__frames[-1]
+                idx_a, idx_b = self.__find_frames_correspondences(frame_candidate, key_frame)
+                points_a = frame_candidate.observed_points[idx_a, :2]
+                points_b = key_frame.observed_points[idx_b, :2]
+                rot, pos, align_error = self.__icp.find_transform(points_a, points_b)
+
+                if align_error <= self.__frame_align_error:
+                    # add a new key frame
+                    frame_candidate.position = pos
+                    frame_candidate.rotation = rot
+                    self.__frames.append(frame_candidate)
+                else:
+                    print("Failed to align frame")
+
             else:
                 self.__frames.append(frame_candidate)
 
@@ -49,10 +72,14 @@ class FrontEnd:
         """
         # Clear the occupancy map
         self.__local_map = np.full((self.__h, self.__w), 255, dtype=np.uint8)
-        current_pos = np.array([0, 0, 1])
+        current_pos = np.array([0., 0., 1.])
+        current_dir = np.array([1., 0., 1.])
         prev_pos = None
         for frame in self.__frames:
-            current_pos = current_pos.dot(frame.transform.T)
+            current_dir = current_dir.dot(frame.rotation.T)
+            current_pos[:2] += current_dir[:2] * np.linalg.norm(frame.position, ord=2)
+
+
             # draw position
             position = to_screen_coords(self.__h, self.__w, current_pos[:2])
             if prev_pos is not None:

playground/slam/icp.py
@@ -1,7 +1,9 @@
+import math
+
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
-from playground.utils.transform import transform_points
+from playground.utils.transform import transform_points, create_rotation_matrix_yx, create_rotation_matrix_2xy
 
 
 class ICP:
@@ -19,65 +21,64 @@ class ICP:
         distances, indices = estimator.kneighbors(points_a, return_distance=True)
         return distances.ravel(), indices.ravel()
 
-    def __fit_transform(self, points_a, points_b):
-        assert points_a.shape == points_b.shape
+    def find_transform(self, points_a, points_b, iterations=100, tolerance=1):
+        # swap x - y
+        points_a = points_a[:, ::-1]
+        points_b = points_b[:, ::-1]
 
-        # move points to their centroids
-        center_a = np.mean(points_a, axis=0)
-        center_b = np.mean(points_b, axis=0)
-        centered_a = points_a - center_a
-        centered_b = points_b - center_b
+        # initial values for tx, ty, angle
+        params = np.array([0.0, 0.0, 0.0])
 
-        # compute the rotation matrix
-        h = np.dot(centered_a.T, centered_b)
-        u, s, vt = np.linalg.svd(h)
-        rot = np.dot(vt.T, u.T)
+        # adjusted_points = points_a.copy()
+        for i in range(iterations):
+            h_sum = np.zeros((3, 3))
+            b_sum = np.zeros(3)
 
-        # reflection case
-        if np.linalg.det(rot) < 0:
-            vt *= -1
-            rot = np.dot(vt.T, u.T)
+            # modify points with params
+            angle = params[2]
+            rot = create_rotation_matrix_2xy(angle)
+            adjusted_points = points_a.dot(rot.T)
+            adjusted_points += params[:2]
 
-        # compute translation
-        pos = center_b - center_a.dot(rot.T)
+            # test if we can stop
+            distances = self.__get_distances(adjusted_points, points_b)
+            mean_error = np.mean(distances)
+            if mean_error < tolerance:
+                break
 
-        # homogeneous transformation
-        tr = np.identity(3)
-        tr[:, :] = rot
-        tr[:2, 2] = pos[:2]
+            for pa, pb, pm in zip(points_a, points_b, adjusted_points):
+                # Jacobian
+                j = np.array([[1, 0, -math.sin(angle) * pa[0] - math.cos(angle) * pa[1]],
+                              [0, 1, math.cos(angle) * pa[0] - math.sin(angle) * pa[1]]])
 
-        return tr, rot, pos
+                # Hesian aproximation
+                h = j.T @ j
+
+                # Right hand side
+                e = pm - pb
+                b = j.T @ e
+
+                # accumulate
+                h_sum += h
+                b_sum += b
+
+            params_update = -np.linalg.pinv(h_sum) @ b_sum
+            params += params_update
+
+        # Calculate an error
+        rot = create_rotation_matrix_2xy(params[2])
+        adjusted_points = points_a.dot(rot.T)
+        adjusted_points += params[:2]
+        distances = self.__get_distances(adjusted_points, points_b)
+        mean_error = np.mean(distances)
+
+        # make result
+        rot3 = create_rotation_matrix_yx(np.degrees(params[2]))
+        pos = params[:2][::-1]
+
+        return rot3, pos, mean_error
 
     def __get_distances(self, points_a, points_b):
         assert points_a.shape == points_b.shape
         distances = np.linalg.norm(points_a - points_b, axis=1)
         return distances
-
-    def find_transform(self, frame_a, frame_b, indices=None):
-        """
-        Finds relative transformation between frames scans in local coordinates
-        """
-        # make points homogeneous
-        points_a = np.hstack([frame_a.observed_points, np.ones((frame_a.observed_points.shape[0], 1))])
-        points_b = np.hstack([frame_b.observed_points, np.ones((frame_b.observed_points.shape[0], 1))])
-
-        # hold initial points_a
-        points_initial = points_a.copy()
-
-        prev_error = 0
-        for i in range(self.__max_iterations):
-            if indices is None:
-                distances, indices = self.__nearest_neighbors(points_a, points_b)
-            else:
-                distances = self.__get_distances(points_a, points_b[indices, :])
-            mean_error = np.mean(distances)
-            if np.abs(prev_error - mean_error) < self.__tolerance:
-                break
-            else:
-                tr, _, _ = self.__fit_transform(points_a, points_b[indices, :])
-                points_a = points_a.dot(tr.T)
-            prev_error = mean_error
-
-        _, rot, pos = self.__fit_transform(points_initial, points_a)
-
-        return rot, pos[:2], prev_error

playground/utils/transform.py
@@ -1,12 +1,21 @@
 import numpy as np
 
 
-def create_rotation_matrix(angle):
+def create_rotation_matrix_yx(angle_degrees):
     mat = np.identity(3)
-    cos_value = np.cos(np.radians(angle))
-    sin_value = np.sin(np.radians(angle))
-    mat[:2, :2] = np.array(((cos_value, -sin_value),
-                            (sin_value, cos_value)))
+    cos_value = np.cos(np.radians(angle_degrees))
+    sin_value = np.sin(np.radians(angle_degrees))
+    mat[:2, :2] = np.array(((cos_value, sin_value),
+                            (-sin_value, cos_value)))
+    return mat
+
+
+def create_rotation_matrix_2xy(angle):
+    mat = np.identity(2)
+    cos_value = np.cos(angle)
+    sin_value = np.sin(angle)
+    mat[:, :] = np.array(((cos_value, -sin_value),
+                          (sin_value, cos_value)))
     return mat
 
 

tests/icp-test.py
@@ -1,8 +1,7 @@
 import unittest
 import numpy as np
 from playground.slam.icp import ICP
-from playground.slam.frame import Frame
-from playground.utils.transform import create_rotation_matrix
+from playground.utils.transform import create_rotation_matrix_yx
 from playground.utils.transform import transform_points
 
 
@@ -17,44 +16,10 @@ class ICPTests(unittest.TestCase):
         offset = np.array([2, 2])
         points_b = points_a + offset
 
-        frame_a = Frame(np.array([0, 0, 1]), np.identity(3), points_a)
-        frame_b = Frame(np.array([0, 0, 1]), np.identity(3), points_b)
-
         icp = ICP()
-        rot, pos, error = icp.find_transform(frame_a, frame_b)
+        rot, pos, error = icp.find_transform(points_a, points_b)
         self.assertTrue(np.allclose(offset, pos))
         self.assertTrue(np.allclose(np.identity(3), rot))
-
-    def test_rotation_manual_index(self):
-        points_a = np.array([[0, 0],
-                             [0, 5],
-                             [0, 10],
-                             [0, 15]])
-
-        rotation = create_rotation_matrix(-45)
-        center_a = np.mean(points_a, axis=0)
-        move = np.identity(3)
-        move[:2, 2] = -center_a
-        move_back = np.identity(3)
-        move_back[:2, 2] = center_a
-        tr = np.matmul(move_back, rotation)
-        tr = np.matmul(tr, move)
-
-        points_b = transform_points(points_a, tr, target_type=float)
-
-        indices = np.array([0, 1, 2, 3])
-
-        frame_a = Frame(np.array([0, 0, 1]), np.identity(3), points_a)
-        frame_b = Frame(np.array([0, 0, 1]), np.identity(3), points_b)
-
-        icp = ICP()
-        rot, pos, error = icp.find_transform(frame_a, frame_b, indices)
-
-        points_a = transform_points(points_a, rot, target_type=float)
-        points_a = points_a.astype(float) + pos
-
-        self.assertTrue(np.allclose(points_a, points_b))
-        self.assertTrue(np.allclose(rotation, rot))
 
     def test_rotation(self):
         points_a = np.array([[0, 0],
@@ -62,7 +27,7 @@ class ICPTests(unittest.TestCase):
                              [0, 10],
                              [0, 15]])
 
-        rotation = create_rotation_matrix(-45)
+        rotation = create_rotation_matrix_yx(-45)
         center_a = np.mean(points_a, axis=0)
         move = np.identity(3)
         move[:2, 2] = -center_a
@@ -73,17 +38,14 @@ class ICPTests(unittest.TestCase):
 
         points_b = transform_points(points_a, tr, target_type=float)
 
-        frame_a = Frame(np.array([0, 0, 1]), np.identity(3), points_a)
-        frame_b = Frame(np.array([0, 0, 1]), np.identity(3), points_b)
-
         icp = ICP()
-        rot, pos, error = icp.find_transform(frame_a, frame_b)
+        rot, pos, error = icp.find_transform(points_a, points_b)
 
         points_a = transform_points(points_a, rot, target_type=float)
         points_a = points_a + pos
 
-        self.assertTrue(np.allclose(points_a, points_b))
-        self.assertTrue(np.allclose(rotation, rot))
+        self.assertTrue(np.allclose(points_a, points_b, rtol=0, atol=0.5))
+        self.assertTrue(np.allclose(rotation, rot, rtol=0, atol=0.5))
 
     def test_rotation_translation(self):
         points_a = np.array([[0, 0],
@@ -91,7 +53,7 @@ class ICPTests(unittest.TestCase):
                              [0, 10],
                              [0, 15]])
 
-        rotation = create_rotation_matrix(-45)
+        rotation = create_rotation_matrix_yx(-45)
         center_a = np.mean(points_a, axis=0)
         move = np.identity(3)
         move[:2, 2] = -center_a
@@ -106,19 +68,45 @@ class ICPTests(unittest.TestCase):
 
         points_b = transform_points(points_a, tr, target_type=float)
 
-        frame_a = Frame(np.array([0, 0, 1]), np.identity(3), points_a)
-        frame_b = Frame(np.array([0, 0, 1]), np.identity(3), points_b)
-
-        # unfortunately knn search fails in the current case, so setup indices manually
-        indices = np.array([0, 1, 2, 3])
         icp = ICP()
-        rot, pos, error = icp.find_transform(frame_a, frame_b, indices)
+        rot, pos, error = icp.find_transform(points_a, points_b)
 
         points_a = transform_points(points_a, rot, target_type=float)
         points_a = points_a + pos
 
-        self.assertTrue(np.allclose(points_a, points_b))
-        self.assertTrue(np.allclose(rotation, rot))
+        self.assertTrue(np.allclose(points_a, points_b, rtol=0, atol=0.5))
+        self.assertTrue(np.allclose(rotation, rot, rtol=0, atol=0.5))
+
+    def test_rotation_translation_non_linear(self):
+        points_a = np.array([[0, 0],
+                             [0, 5],
+                             [0, 10],
+                             [0, 15]])
+
+        rotation = create_rotation_matrix_yx(-45)
+        center_a = np.mean(points_a, axis=0)
+        move = np.identity(3)
+        move[:2, 2] = -center_a
+        move_back = np.identity(3)
+        move_back[:2, 2] = center_a
+        move_add = np.identity(3)
+        move_add[:2, 2] = [2., 2.]
+
+        tr = np.matmul(move_add, move_back)
+        tr = np.matmul(tr, rotation)
+        tr = np.matmul(tr, move)
+
+        points_b = transform_points(points_a, tr, target_type=float)
+
+        icp = ICP()
+        rot, pos, error = icp.find_transform(points_a, points_b)
+
+        points_a = transform_points(points_a, rot, target_type=float)
+        points_a = points_a + pos
+
+        # tolerance is 0.5 pixel
+        self.assertTrue(np.allclose(points_a, points_b, rtol=0, atol=0.5))
+        self.assertTrue(np.allclose(rotation, rot, rtol=0, atol=0.5))
 
 
 if __name__ == '__main__':